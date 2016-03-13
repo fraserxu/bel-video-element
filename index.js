b8ec34b7b63e2b1dd6ae7e5519bb5aa3fc710ba4
fraserxu <xvfeng123@gmail.com>
2016-03-13 12:00:59 +1100

    fix test

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var bel = require('bel')
 
 module.exports = function (opts) {
-  var opts = opts || {}
+  opts = opts || {}
   var width = opts.width || 480
   var styles = opts.styles || ''
 
-  var controls = !!!opts.controls
+  var controls = !opts.controls
   var poster = opts.poster || ''
   var mp4Src = opts.mp4Src ? bel`<source src=${opts.mp4Src} type="video/mp4">` : ''
   var oggSrc = opts.oggSrc ? bel`<source src=${opts.oggSrc} type="video/ogg">` : ''
